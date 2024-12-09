6026286c22ea8b8af405a24605875649734c1097
HeliosC <christophe.sikes@outlook.com>
2024-12-10 00:42:37 +0100

    Day template

---
+++ src/day00/Day00.kt
@@ -0,0 +1,29 @@
+package day00
+
+import println
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        var score = 0
+
+        return score
+    }
+
+    fun part2(input: List<String>): Int {
+        var score = 0
+
+        return score
+    }
+
+    val testInput = readInput("00", testData = true)
+    val input = readInput("00")
+
+    part1(testInput).println()
+    //part2(testInput).println()
+
+    //check(part1(testInput) == 0)
+    //check(part2(testInput) == 0)
+    //check(part1(input) == 0)
+    //check(part2(input) == 0)
+}
